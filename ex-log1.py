51a04eb44deb8dd03e42042192d2e2b229b2e575
Viller Hsiao <villerhsiao@gmail.com>
2012-11-02 17:16:21 +0800

    python: add logging example
    log to file

---
+++ ex-log1.py
@@ -0,0 +1,13 @@
+
+
+import logging
+plog = logging.getLogger('myapp')
+pfile = logging.FileHandler('foo.log')
+pfmt = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+pfile.setFormatter(pfmt)
+plog.addHandler(pfile) 
+plog.setLevel(logging.WARNING)
+
+plog.error('Show error')
+plog.warn('Show warning')
+plog.info('Show info')
