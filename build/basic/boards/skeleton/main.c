e2bb756842398cbb535f6d389711fe0eeba6e184
Viller Hsiao <villerhsiao@gmail.com>
2013-08-14 23:58:42 +0800

    build: basic: restruct directories

---
+++ build/basic/boards/skeleton/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <foo.h>
+
+int
+main(int argc, void **argv)
+{
+    printf("hello world\n");
+    foo();
+    return 0;
+}
--- build/basic/mk/config.mk
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2013 The F9 Microkernel Project. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-AUTOCONF_H := include/autoconf.h
-config_list := $(shell [ -f $(AUTOCONF_H) ] || touch $(AUTOCONF_H) ; $(CC) -E -dM include/config.h | \
-	grep -o "\#define CONFIG_[A-Za-z0-9_]*" | cut -c9- | sort)
-$(foreach c,$(config_list) $(config_list),$(eval $(c)=y))
